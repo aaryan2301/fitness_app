bfb3fb862c748317d44147db54ded0af63bf8742
Aaryan Bhaaii <20uec035@lnmiit.ac.in>
2023-11-11 20:58:02 +0530

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart] = useState('all');
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
+
+export default Home;
